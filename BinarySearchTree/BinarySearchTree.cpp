689c380a006d7e6b307b85c1a68d88c74891c7fb
anjanipodomi <anjanidihapsari@gmail.com>
2024-06-13 10:55:08 +0700

    membuat class node dengan string info, node* lightchild dan node right child

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -5,9 +5,22 @@ using namespace std;
 class Node
 {
 public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
 
+	// Connstruktor for the node class
+	Node(string i, Node* l, Node* r) {
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
 };
 
+class BinaryTree
+{
+
+};
 
 int main()
 {
