e5634739942438d5254aeab34548370de7d55e9d
anjanipodomi <anjanidihapsari@gmail.com>
2024-06-13 11:25:25 +0700

    membuat void inorder. jika root=null tampilkan "Tree is empty". jika ptr tidak sama dengan null  inorder(ptr->leftchild);  cout << ptr->info << " ";  inorder(ptr->rightchild);

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -67,7 +67,21 @@ public:
 				currentNode = currentNode->rightchild;
 		}
 	}
-
+	
+	void inorder(Node* ptr)
+	{
+		if (ROOT == nullptr)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	}
 };
 
 
